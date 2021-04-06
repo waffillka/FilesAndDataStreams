ef5de2123d0e7ef28c74ad5ea714556f58ce2e18
waffillka <waffilka@gmail.com>
2021-04-06 18:14:14 +0300

    Create base UI and base actions

--- FilesAndDataStreams/Form1.cs
+++ FilesAndDataStreams/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,59 @@ namespace FilesAndDataStreams
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void ДанныеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = File.ReadAllText("D:\\templog.txt");
+        }
+
+        private void RenameStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "rename";
+        }
+
+        private void DeleteStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Delete";
+        }
+
+        private void ReadDataStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Read";
+        }
+
+        private void LoggingStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Logging";
+        }
+
+        private void AuthorStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Author";
+        }
+
+        private void CopyStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Copy";
+        }
+
+        private void ArchiveStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Archive";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
     }
 }
